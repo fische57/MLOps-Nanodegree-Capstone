e03a5fc2340419502ce04cfe104a6715dac0549e
Maria Fischer <52047242+fische57@users.noreply.github.com>
2023-09-01 15:23:39 -0400

    Update Score2.py

--- capstone-9-1/Score2.py
+++ capstone-9-1/Score2.py
@@ -1,5 +1,6 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
+# Modifications based on this Github repository: https://github.com/ons-cherif/Covid19-New-Cases-Classification.git
 # ---------------------------------------------------------
 import json
 import logging
